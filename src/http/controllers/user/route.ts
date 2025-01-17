79fc66ca9c37478153e5f0650f4a6c43b4934fb3
matheus mendes <matheus_mendess@live.com>
2025-01-17 17:47:01 -0300

    feat: route get user

--- src/http/controllers/user/route.ts
+++ src/http/controllers/user/route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { CreateUserController } from './create-user-controller';
+import UserGetController from './user-get-controller';
+import { AuthenticateMiddleware } from '@/http/middlewares/authenticate-middleware';
 
 const userRoute = Router();
 
 userRoute.post('/user', CreateUserController);
+userRoute.get('/user/:id', AuthenticateMiddleware, UserGetController);
 
 export default userRoute;
--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,6 +3,12 @@ import { Prisma, User } from '@prisma/client';
 import { UserRepository } from '../user-repository';
 
 export class PrismaUserRepository implements UserRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    return user;
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({ where: { email } });
 
