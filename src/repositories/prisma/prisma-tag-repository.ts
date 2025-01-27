21ac3ef93c5013f896155d22c3dbb168c7210561
matheus mendes <matheus_mendess@live.com>
2025-01-27 15:09:37 -0300

    feat pagination tags

--- src/repositories/prisma/prisma-tag-repository.ts
+++ src/repositories/prisma/prisma-tag-repository.ts
@@ -3,10 +3,22 @@ import { TagRepository } from '../tag-repository';
 import { prisma } from '@/lib/prisma';
 
 export class PrismaTagRepository implements TagRepository {
-  async list(): Promise<Tag[]> {
-    const tags = await prisma.tag.findMany({});
+  async list(
+    offset: number = 1,
+    limit: number = 25
+  ): Promise<{ totalCount: number; hasMore: boolean; offset: number; limit: number; tags: Tag[] }> {
+    const count = await prisma.tag.count();
 
-    return tags;
+    const tags = await prisma.tag.findMany({
+      take: limit,
+      skip: (offset - 1) * limit,
+    });
+
+    const totalPages = Math.ceil(count / limit);
+
+    const hasMore = offset < totalPages;
+
+    return { totalCount: count, hasMore, offset, limit, tags };
   }
   async create(data: Prisma.TagCreateInput): Promise<Tag> {
     const tag = await prisma.tag.create({ data });
