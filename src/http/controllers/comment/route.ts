3cd723ce2d07aac1b815a3d485069716f8963307
matheus mendes <matheus_mendess@live.com>
2025-02-26 10:19:59 -0300

    add rate limit middleware

--- src/http/controllers/comment/route.ts
+++ src/http/controllers/comment/route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { CreateCommentController } from './create-comment-controller';
+import { commentRateLimiter } from '@/http/middlewares/rate-limit-middleware';
 
 const commentRoute = Router();
 
-commentRoute.post('/comment/:postId', CreateCommentController);
+commentRoute.post('/comment/:postId', commentRateLimiter, CreateCommentController);
 
 export default commentRoute;
