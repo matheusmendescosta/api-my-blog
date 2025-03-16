2cad1e7347114da2fbc243005896309eb3422455
Matheus Mendes <matheus_mendess@live.com>
2025-03-16 12:13:41 -0300

    fix auth error

--- src/lib/nodemailer.tsx
+++ src/lib/nodemailer.tsx
@@ -7,10 +7,12 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
+  auth: process.env.SMTP_USER
+    ? {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      }
+    : undefined,
   tls: {
     ciphers: 'SSLv3',
     rejectUnauthorized: false,
